0332a8a4c1084547afc9848f6209a95c7369b2be
lohug1 <arturo.tapia.r.95@gmail.com>
2024-07-05 17:32:33 -0600

    commentary

--- src/colorbar.jsx
+++ src/colorbar.jsx
@@ -11,27 +11,29 @@ export function ColorBar({target_hue, threshold}){
             var pos = x * 4;
             let interval = [target_hue - threshold, target_hue + threshold];
             let isOverflow = false;
-            if(interval[0] < 0){ 
+            if(interval[0] < 0){ //Vemos si el intervalo es negativo
                 let aux = interval[1];
-                interval[1] = interval[0] + 360;
+                interval[1] = interval[0] + 360; //Obtenemos el valor correspondiente
                 interval[0] = aux;
-                isOverflow = true;
+                isOverflow = true; //Marcamos que hubo un overflow
             }
-            if(interval[1] > 360){ 
+            if(interval[1] > 360){ //Vemos si el intervalo es mayor a 360
                 let aux = interval[1];
                 interval[1] = interval[0];
-                interval[0] = aux % 360;
-                isOverflow=true;
+                interval[0] = aux % 360; //Obtenemos el valor correspondiente
+                isOverflow=true; //Marcamos que hubo un overflow
             }
-            if((interval[0] < x / width * 360 && interval[1] > (x / width * 360)) != isOverflow){
-                row[pos] = 255;           // some R value [0, 255]
-                row[pos+1] = 255;           // some G value
-                row[pos+2] = 255;           // some B value
+
+            //usamos isOverflow como un XOR para corregir el condicional 
+            if(isOverflow != (interval[0] < x / width * 360 && interval[1] > (x / width * 360))){
+                row[pos] = 255;           // R
+                row[pos+1] = 255;           // G
+                row[pos+2] = 255;           // B
                 row[pos+3] = 255;    
             }else{
-                row[pos] = 0;           // some R value [0, 255]
-                row[pos+1] = 0;           // some G value
-                row[pos+2] = 0;           // some B value
+                row[pos] = 0;           // R
+                row[pos+1] = 0;           //G
+                row[pos+2] = 0;           //B
                 row[pos+3] = 255;    
             }
         }
